1ab39f1e997235c0c4cd2417c93cf0ea13a68658
Laura Steiner <steiner.laura@gmail.com>
2021-03-18 01:10:16 -0700

    Adds cypress test and initial workflow.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     "es6": true,
     "node": true,
     "commonjs": true,
+    "cypress/globals": true,
     "jest/globals": true
   },
   "extends": [
@@ -18,7 +19,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
