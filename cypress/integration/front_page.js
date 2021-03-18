bebd9c065ceb694759b3dd007b6821ba550438f3
Laura Steiner <steiner.laura@gmail.com>
2021-03-18 01:30:50 -0700

    Adds cypress test going to a pokemon page.

--- cypress/integration/front_page.js
+++ cypress/integration/front_page.js
@@ -3,5 +3,7 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    cy.get('a[href*="pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
   })
 })
